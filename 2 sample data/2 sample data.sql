5d8a63a432838133e24df433058cee81ba8fbd30
BZaidman <benzionzaidman@gmail.com>
2025-01-27 14:42:02 -0500

    view results

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,7 +1,8 @@
+--BZ fill data for planet
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'muoth', 2
+union select 'muoth', 2 -- BZ What's this?
 union select 'teeth', 32
 union select 'toungue', 1
--- 2 sample data/planets.sql
+++ 2 sample data/planets.sql
@@ -1,3 +1,4 @@
+--BZ move to data
 earth
 Mars
 jupiter
